3a81a911945efe02489c72adff6fd79d942fa527
Pradnya Bhondivale <pradnyabhondivale@gmail.com>
2026-07-09 11:49:42 +0530

    Time: 3 ms (45.47%) | Memory: 10.4 MB (46.24%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0,y=0;
+        for(char move : moves)
+        {
+            if(move=='U')
+                y++;
+            else if(move=='D')
+                y--;
+            else if(move=='R')
+                x++;
+            else
+                x--;
+        }
+        return(x==0 && y==0);
+    }
+};
