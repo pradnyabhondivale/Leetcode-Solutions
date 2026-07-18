0ec9be7f70270d62c3b1bbef3eda386770e07b69
Pradnya Bhondivale <pradnyabhondivale@gmail.com>
2026-07-18 21:32:22 +0530

    Time: 4 ms (67.01%) | Memory: 9.9 MB (94.45%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
+    int value(char c)
+    {
+        switch(c)
+        {
+            case 'I': return 1;
+            case 'V' : return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
     int romanToInt(string s) {
-        unordered_map<char,int> mp;
-        mp['I'] = 1;
-        mp['V'] = 5;
-        mp['X'] = 10;
-        mp['L'] = 50;
-        mp['C'] = 100;
-        mp['D'] = 500;
-        mp['M'] = 1000;
+        // unordered_map<char,int> mp;
+        // mp['I'] = 1;
+        // mp['V'] = 5;
+        // mp['X'] = 10;
+        // mp['L'] = 50;
+        // mp['C'] = 100;
+        // mp['D'] = 500;
+        // mp['M'] = 1000;
 
         int ans = 0;
         for(int i=0;i<s.length();i++)
         {
-            if(i<s.length()-1 && mp[s[i]]<mp[s[i+1]])
-                ans -= mp[s[i]];
+            if(i<s.length()-1 && value(s[i])<value(s[i+1]))
+                ans -= value(s[i]);
             else
-                ans += mp[s[i]];
+                ans += value(s[i]);
         }
         return ans;
     }
